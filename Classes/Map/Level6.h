fd5952613dfdd0369ac871ab77692708e4cf5664
amazarashi <241880152@smail.nju.edu.cn>
2025-11-11 15:37:32 +0800

    更新了后续七关的内容；后续关卡的完成暂定

---
+++ Classes/Map/Level6.h
@@ -0,0 +1,27 @@
+//
+// Created by 20619 on 2025/11/11.
+//
+
+#ifndef KINGDOMRUSH_STU_LEVEL6_H
+#define KINGDOMRUSH_STU_LEVEL6_H
+
+#include "Map/BaseMap.h"
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class Level6 : public BaseMap{
+public:
+    Level6();
+    ~Level6();
+    static Level6* createGame(int difficulty);
+    virtual bool initWithDifficulty(int difficulty);
+private:
+    void addOrnament() override;
+    void onExit() override;
+    void addTerrains() override;
+};
+
+
+
+#endif //KINGDOMRUSH_STU_LEVEL6_H
---
+++ Classes/Map/Level8.h
@@ -0,0 +1,24 @@
+//
+// Created by 20619 on 2025/11/11.
+//
+
+#ifndef KINGDOMRUSH_STU_LEVEL8_H
+#define KINGDOMRUSH_STU_LEVEL8_H
+
+#include "Map/BaseMap.h"
+#include "cocos2d.h"
+USING_NS_CC;
+
+class Level8 : public BaseMap{
+public:
+    Level8();
+    ~Level8();
+    static Level8* createGame(int difficulty);
+    virtual bool initWithDifficulty(int difficulty);
+private:
+    void addOrnament() override;
+    void onExit() override;
+    void addTerrains() override;
+};
+
+#endif //KINGDOMRUSH_STU_LEVEL8_H
